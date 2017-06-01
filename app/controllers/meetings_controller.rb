f49f1dd9df55ac2fc9ec4ec7789ecb770af8245c
GabAlexandria <gabagrant@gmail.com>
2017-05-31 22:35:52 -0400

    add smart listing to events

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,9 +1,10 @@
 class MeetingsController < EventsController
-  def index
-    @events = Meeting.all
-  end
 
   private
+    def scope
+      params[:filter].blank? ? Meeting.all : Meeting.like(params[:filter])
+    end
+
     def event_params
       params.require(:meeting).permit(:name, :event_start, :event_end, :event_type, :location, :notes, :other_attendee_count, :type)
     end
