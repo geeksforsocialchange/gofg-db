e9325a85c5363052eb320b4e86fbfe85d6cb1db9
GabAlexandria <gabagrant@gmail.com>
2017-07-05 00:00:10 -0400

    set up 2FA logic and page

---
+++ app/controllers/user_mfa_session_controller.rb
@@ -0,0 +1,20 @@
+class UserMfaSessionController < ApplicationController
+
+  skip_before_action :check_mfa
+  def new
+
+  end
+
+  def create
+    user = current_user
+
+    if user.google_authentic?(params[:user_mfa_session][:code])
+      UserMfaSession.create(user)
+      flash.notice = 'You have been successfully signed in'
+      redirect_to root_path
+    else
+      flash[:alert] = 'Incorrect code. Please try again.'
+      render :new
+    end
+  end
+end
