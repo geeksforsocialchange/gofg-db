2caf16540e4018d4f2845da7e6fd2912a37896b2
GabAlexandria <gabagrant@gmail.com>
2018-05-18 09:38:35 -0400

    require participants and mentors for peers, fix peers index page

--- app/models/peer.rb
+++ app/models/peer.rb
@@ -2,5 +2,7 @@ class Peer < ApplicationRecord
   belongs_to :participant, foreign_key: :participant_id, class_name: 'Person'
   belongs_to :mentor
 
+  validates :participant_id, :mentor_id, presence: true
+
   scope :by_mentor, ->(filter) { where(mentor_id: filter) }
 end
